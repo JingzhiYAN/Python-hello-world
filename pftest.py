1cc7783cb4977a0d693ccfc594ca476d581a0d9c
JingzhiYAN <bioyanjingzhi@gmail.com>
2017-12-28 09:03:54 +0800

    semi-auto done

--- pftest.py
+++ pftest.py
@@ -10,13 +10,13 @@ import time
 import random
 
 
-a = open('categorylist.txt','r')
+a = open('cate3.txt','r')
 while True:
     line = a.readline()
     if not line:
         break
     if line is None:
-        f=open("record.txt","a")
+        f=open("record3.txt","a")
         f.write('\n')
         print('\n')
         f.close()
@@ -29,16 +29,12 @@ while True:
     c= b"Your search returns no results."
 
     if c in html:
-        f=open("record.txt","a")
-        f.write('true')
+        f=open("record3.txt","a")
+        f.write(line)
         f.write('\n')
-        print(line)
+        print('not exist')
         f.close()
         time.sleep(0.5)
     else:
-        f=open("record.txt","a")
-        f.write('false')
-        f.write('\n')
-        print('\n')
-        f.close()
-        time.sleep(0.5)
+        print('exist')
+        print(line)
